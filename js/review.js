aaab4f973dc45c998878c3fbf77321f3544e30ab
dthoang <dthoang@uw.edu>
2015-10-28 04:17:46 -0700

    Backing up before possible major changes. Added up and down voting and deleting

--- js/review.js
+++ js/review.js
@@ -5,26 +5,29 @@ $(document).ready(function() {
 	// Create a new sub-class of the Parse.Object, with name "Review"
 	var Review = Parse.Object.extend('Review');
 
+	var query = new Parse.Query(Review)
+
 	$('#starRate').raty({
-			numberMax: 5
+		numberMax: 5
 	});
 
-
-	// Saves reviewer's data to Parse
+	// Saves new reviewer's data to Parse
 	$('form').submit(function() {;
 		var review = new Review()
 
 		var title = $('#review-title').val();
 		review.set('title', title);
+
 		var opinion = $('#opinion').val();
 		review.set('opinion', opinion);
+
 		var starRate = $('#starRate').raty('score');
 		starRate = Number(starRate);
 		review.set('Rating', starRate);
 
 		//For up and down votes
-		review.set("upVote", 0);
-		review.set("votes", 0);
+		review.set("upVotes", 0);
+		review.set("downVotes", 0);
 			
 		review.save(null, {
 			success:getData
@@ -32,11 +35,8 @@ $(document).ready(function() {
 		return false
 	});
 
-
-
 	// Write a function to get data
 	var getData = function() {
-		var query = new Parse.Query(Review)
 		query.notEqualTo('opinion', '')
 		query.find({
 			success:function(results) {
@@ -48,18 +48,18 @@ $(document).ready(function() {
 	// A function to build your list
 	var buildList = function(data) {
 		$('ol').empty()
-		var totalRating = 0;
-		var totalReview = 0;
+
+		var totalRating = 0;	
 
 		data.forEach(function(d){
+			totalRating += d.get('Rating');
 			addItem(d);
-			// totalRating += item.get('starRate');
-			// totalReview += 1;
 		});
-		// $('#avgRate').raty({
-		// 	score: totalRating/totalReview,
-		// 	readOnly: true
-		// });
+
+		$('#avgRate').raty({
+			readOnly: true,
+			score: totalRating/(data.length)
+		})	
 	}
 
 
@@ -69,30 +69,59 @@ $(document).ready(function() {
 		var title = item.get('title');
 		var div = ('<div id="ReviewDiv"></div>');
 		var rate = item.get('Rating');
-		
+		var upVotes = item.get('upVotes');
+		var downVotes = item.get('downVotes');
 		var li = $('<li><h3>' + title + '</h3>' + opinion + '</li>');
+
+		//vote up and down buttons and functions
+		var downVotes = $('<button id="thumbDown" class="button fa fa-thumbs-o-down"></button>');
+		var upVotes = $('<button id="thumbUp" class ="button fa fa-thumbs-o-up"></button>');
+
+        upVotes.click(function() {
+            query.get(item.id, {
+                success: function(review) {
+                    review.increment('upVotes')
+                    review.save(null, {
+                        success: getData
+                    });
+                }
+            })
+        })
+
+        downVotes.click(function() {
+            query.get(item.id, {
+                success: function(review) {
+                    review.increment('downVotes')
+                    review.save(null, {
+                        success: getData
+                    });
+                }
+            })
+        })
+
+		upVotes.appendTo(li);
+		downVotes.appendTo(li);
+		var totalVotes = upVotes + downVotes;
+
+		//Creates a button to "destroy" comments, removing them from Parse memory
+		var destroyButton = $('<button><i class="button fa fa-icon-remove-sign "></i></button>');
+		destroyButton.click(function() {
+			item.destroy({
+				success:getData
+			})
+		})
+
+		destroyButton.appendTo(li);
+
 		$('ol').append(li);
 		$('ol').append(div);
 		$('div:last').raty({
 		 	readOnly: true,
 		 	score: rate
 		});
-	}
+		$('ol').append(Number(upVotes) + " people out of " + Number(totalVotes) + " liked this review!");
+	};
 
 	// Call your getData function when the page loads
-	getData()
+	getData();
 });
-
-
-		/*
-		// Create a button with a <span> element (using bootstrap class to show the X)
-		var button = $('<button class="btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span></button>')
-		
-		// Click function on the button to destroy the item, then re-call getData
-		button.click(function() {
-			item.destroy({
-				success:getData
-			})
-		})
-
-		li.append(button); */
