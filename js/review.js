7e89759086ba7e5ebdf4e8c862803f6603f5a1ea
dthoang <dthoang@uw.edu>
2015-10-28 00:10:52 -0700

    Able to submit user reviews

--- js/review.js
+++ js/review.js
@@ -1,92 +1,98 @@
-//Initialize Parse App
-var appID = 'gMrBRWKQAjM0wqD7EiNJPRXl8mRk1Wepv93JoMl3';
-var jID = 'zdtInzpnas4ueqwOrpnJbW6NU7MqY0ta3jdzYAXK';
-Parse.initialize(appID, jID);
+$(document).ready(function() {
+	//Initialize Parse App
+	Parse.initialize('gMrBRWKQAjM0wqD7EiNJPRXl8mRk1Wepv93JoMl3', 'zdtInzpnas4ueqwOrpnJbW6NU7MqY0ta3jdzYAXK');
 
-// Create a new sub-class of the Parse.Object, with name "Review"
-var Review = Parse.Object.extend('Review');
+	// Create a new sub-class of the Parse.Object, with name "Review"
+	var Review = Parse.Object.extend('Review');
 
-
-// Click event when form is submitted
-$('form').submit(function() {
-
-	// Create a new instance of your Music class 
-	var review = new Review();
-
-	// For each input element, set a property of your new instance equal to the input's value
-	
-	$(this).find('input').each(function(){
-		review.set($(this).attr('id'), $(this).val());
-		$(this).val('');
-	})
-
-	// After setting each property, save your new instance back to your database
-	review.save(null, {
-		success:getData
-	})
-	return false
-})
+	$('#starRate').raty({
+			numberMax: 5
+	});
 
 
+	// Saves reviewer's data to Parse
+	$('form').submit(function() {;
+		var review = new Review()
 
-// Write a function to get data
-var getData = function() {
-	
+		var title = $('#review-title').val();
+		review.set('title', title);
+		var opinion = $('#opinion').val();
+		review.set('opinion', opinion);
+		var starRate = $('#starRate').raty('score');
+		starRate = Number(starRate);
+		review.set('Rating', starRate);
 
-	// Set up a new query for our Review class
-	var query = new Parse.Query(Review)
-
-	// Set a parameter for your query -- where the website property isn't missing
-	query.notEqualTo('opinion', '')
-
-	/* Execute the query using ".find".  When successful:
-	    - Pass the returned data into your buildList function
-	*/
-	query.find({
-		success:function(results) {
-			buildList(results)
-		} 
-	})
-}
-
-// A function to build your list
-var buildList = function(data) {
-	// Empty out your ordered list
-	$('ol').empty()
-
-	// Loop through your data, and pass each element to the addItem function
-	data.forEach(function(d){
-		addItem(d);
-	})
-}
-
-
-// This function takes in an item, adds it to the screen
-var addItem = function(item) {
-	// Get parameters (website, band, song) from the data item passed to the function
-	var opinion = item.get('opinion')
-	var reviewTitle = item.get('review-title')
-	
-	/*
-	// Append li that includes text from the data item
-	var li = $('<li>Check out ' + band + ', their best song is ' + song + '</li>')
-	
-	// Create a button with a <span> element (using bootstrap class to show the X)
-	var button = $('<button class="btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span></button>')
-	
-	// Click function on the button to destroy the item, then re-call getData
-	button.click(function() {
-		item.destroy({
+		//For up and down votes
+		review.set("upVote", 0);
+		review.set("votes", 0);
+			
+		review.save(null, {
 			success:getData
+		});
+		return false
+	});
+
+
+
+	// Write a function to get data
+	var getData = function() {
+		var query = new Parse.Query(Review)
+		query.notEqualTo('opinion', '')
+		query.find({
+			success:function(results) {
+				buildList(results)
+			} 
 		})
-	})
+	}
 
-	// Append the button to the li, then the li to the ol
-	li.append(button);
-	$('ol').append(li) */
-	
-}
+	// A function to build your list
+	var buildList = function(data) {
+		$('ol').empty()
+		var totalRating = 0;
+		var totalReview = 0;
 
-// Call your getData function when the page loads
-getData()
+		data.forEach(function(d){
+			addItem(d);
+			// totalRating += item.get('starRate');
+			// totalReview += 1;
+		});
+		// $('#avgRate').raty({
+		// 	score: totalRating/totalReview,
+		// 	readOnly: true
+		// });
+	}
 
+
+	// This function takes in an item, adds it to the screen
+	var addItem = function(item) {
+		var opinion = item.get('opinion');
+		var title = item.get('title');
+		var div = ('<div id="ReviewDiv"></div>');
+		var rate = item.get('Rating');
+		
+		var li = $('<li><h3>' + title + '</h3>' + opinion + '</li>');
+		$('ol').append(li);
+		$('ol').append(div);
+		$('div:last').raty({
+		 	readOnly: true,
+		 	score: rate
+		});
+	}
+
+	// Call your getData function when the page loads
+	getData()
+});
+
+
+		/*
+		// Create a button with a <span> element (using bootstrap class to show the X)
+		var button = $('<button class="btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span></button>')
+		
+		// Click function on the button to destroy the item, then re-call getData
+		button.click(function() {
+			item.destroy({
+				success:getData
+			})
+		})
+
+		li.append(button); */
